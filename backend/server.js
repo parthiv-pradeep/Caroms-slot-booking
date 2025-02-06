2d6890a35a2731419dfee161171b781604e5a642
Parthiv Pradeep <parthivpradeep@mgmcet.ac.in>
2025-02-06 14:22:44 +0530

    the api updated

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,21 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-
-
 // Middleware
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 
 // MongoDB Connection
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://admin:admin@cluster0.2ttr1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Error connecting to MongoDB:", err));
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://admin:admin@cluster0.2ttr1.mongodb.net/carroms?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase connection timeout to 30 seconds
+  socketTimeoutMS: 45000,  // Increase socket timeout to 45 seconds
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("Error connecting to MongoDB:", err));
 
 
 // Routes
