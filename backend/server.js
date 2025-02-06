5352fb291f65406b6d07a82cfac905a785bb2440
Parthiv Pradeep <parthivpradeep@mgmcet.ac.in>
2025-02-06 12:12:22 +0530

    env updated

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 // MongoDB Connection
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://admin:admin@cluster0.2ttr1.mongodb.net/carrom_booking?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://admin:admin@cluster0.2ttr1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
